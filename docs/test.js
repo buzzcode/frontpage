835d5c1677a9e52033e896c4a2208959cfaa7864
Kevin Rise <krise@veracode.com>
2022-05-24 07:02:51 -0400

    try to read json file

--- docs/test.js
+++ docs/test.js
@@ -1,4 +1,7 @@
 
 $().ready(function() {
-    $("#text").html("This is a test of javascript, from test.js");
+    //$("#text").html("This is a test of javascript, from test.js");
+    $.getJSON("../refs/veracode.json", function(data) {
+        console.log(data);
+    })
 });
