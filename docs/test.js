58131b23daa9d8fca215ad438f04f6bd48ae4f5e
Kevin Rise <krise@veracode.com>
2022-05-24 07:12:18 -0400

    move test data under the docs folder

--- docs/test.js
+++ docs/test.js
@@ -1,7 +1,7 @@
 
 $().ready(function() {
     //$("#text").html("This is a test of javascript, from test.js");
-    $.getJSON("../refs/veracode.json", function(data) {
+    $.getJSON("./data/veracode.json", function(data) {
         console.log(data);
     })
 });
